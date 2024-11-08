0464d50087b9d53439388195468704b8f32f5147
samsulmaarif1076 <samsulmaarif1076@gmail.com>
2024-11-08 07:35:45 +0000

    feat(BookingTransactionResource): CRUD data Booking Transaction

--- app/Filament/Resources/BookingTransactionResource.php
+++ app/Filament/Resources/BookingTransactionResource.php
@@ -5,10 +5,22 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\BookingTransactionResource\Pages;
 use App\Filament\Resources\BookingTransactionResource\RelationManagers;
 use App\Models\BookingTransaction;
+use App\Models\Ticket;
 use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ToggleButtons;
+use Filament\Forms\Components\Wizard;
+use Filament\Forms\Components\Wizard\Step;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -23,7 +35,83 @@ class BookingTransactionResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Wizard::make([
+                    Step::make('Product and Price')
+                        ->schema([
+                            Select::make('ticket_id')
+                                ->relationship('ticket', 'name')
+                                ->searchable()
+                                ->preload()
+                                ->required()
+                                ->reactive()
+                                ->afterStateUpdated(function ($state, callable $set) {
+                                    $ticket = Ticket::find($state);
+                                    $set('price', $ticket ? $ticket->price : 0);
+                                }),
+
+                            TextInput::make('total_participant')
+                                ->required()
+                                ->numeric()
+                                ->prefix('people')
+                                ->reactive()
+                                ->afterStateUpdated(function ($state, callable $get, callable $set) {
+                                    $price = $get('price');
+                                    $subTotal = $price * $state;
+                                    $totalPpn = $subTotal * 0.11;
+                                    $totalAmount = $subTotal + $totalPpn;
+                                    $set('total_amount', $totalAmount);
+                                }),
+
+                            TextInput::make('total_amount')
+                                ->required()
+                                ->numeric()
+                                ->prefix('IDR')
+                                ->readOnly()
+                                ->helperText('Harga Sudah Include Ppn 11%'),
+                        ]),
+
+                        Step::make('Customer Information')
+                        ->schema([
+                            TextInput::make('name')
+                                ->required()
+                                ->maxLength(255),
+
+                            TextInput::make('phone_number')
+                                ->required()
+                                ->maxLength(255),
+
+                            TextInput::make('email')
+                                ->required()
+                                ->maxLength(255),
+
+                            TextInput::make('booking_trx_id')
+                                ->required()
+                                ->maxLength(255),
+                        ]),
+
+                       Step::make('Payment Information')
+                       ->schema([
+                        ToggleButtons::make('is_paid')
+                        ->label('Apkah Sudah Membayar?')
+                        ->boolean()
+                        ->grouped()
+                        ->icons([
+                            true => 'heroicon-o-pencil',
+                            false => 'heroicon-o-clock',
+                        ])
+                        ->required(),
+
+                        FileUpload::make('proof')
+                        ->image()
+                        ->required(),
+
+                        DatePicker::make('started_at')
+                        ->required(),
+                        ]),
+                     ])
+                     ->columnSpan('full')
+                     ->columns(1)
+                     ->skippable(),
             ]);
     }
 
@@ -31,13 +119,32 @@ class BookingTransactionResource extends Resource
     {
         return $table
             ->columns([
-                //
+                ImageColumn::make('ticket.thumbnail')
+                ->circular(),
+
+                TextColumn::make('name')
+                ->searchable(),
+
+                TextColumn::make('booking_trx_id')
+                ->searchable(),
+
+                IconColumn::make('is_paid')
+                ->boolean()
+                ->trueColor('success')
+                ->falseColor('danger')
+                ->trueIcon('heroicon-o-check-circle')
+                ->falseIcon('heroicon-o-x-circle')
+                ->label('Terverifikasi')
+
             ])
             ->filters([
-                //
+                SelectFilter::make('ticket_id')
+                ->label('ticket')
+                ->relationship('ticket', 'name'),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
