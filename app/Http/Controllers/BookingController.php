8920a3ee636e11f200b418ce0f9fbfe3c9caa49f
samsulmaarif1076 <samsulmaarif1076@gmail.com>
2024-11-14 09:43:45 +0000

    feat: coding booking controller

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
+use App\Services\BookingService;
+use App\Http\Requests\StoreBookingRequest;
 
 class BookingController extends Controller
 {
     //
+    protected $bookingService;
+
+    public function __construct(BookingService $bookingService)
+    {
+        $this->bookingService = $bookingService;
+    }
+
+    public function booking(Ticket $ticket)
+    {
+        dd($ticket);
+        // return view('front.booking', compact('ticket'));
+    }
+
+    public function storeBooking(Ticket $ticket, StoreBookingRequest $request)
+    {
+        $validated = $request->validated();
+
+        $totals = $this->bookingService->calculateTotals($ticket->id, $validated['total_participant']);
+        $this->bookingService->storeBookingInSession($ticket, $validated, $totals);
+
+        return redirect()->route('front.payment');
+    }
+
+
 }
