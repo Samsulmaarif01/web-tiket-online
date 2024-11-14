f4eb73287a495550618e9efaca6fec58510c2ee9
samsulmaarif1076 <samsulmaarif1076@gmail.com>
2024-11-14 02:13:12 +0000

    feat: make coding front service

---
+++ app/Services/FrontService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\TicketRepositoryInterface;
+use App\Repositories\Contracts\CategoryRepositoryInterface;
+
+class FrontService
+{
+    protected $categoryRepository;
+    protected $ticketRepository;
+
+    public function __construct(TicketRepositoryInterface $ticketRepository, CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+        $this->ticketRepository = $ticketRepository;
+    }
+
+    public function getFrontPageData()
+    {
+        $categories = $this->categoryRepository->getAllCategories();
+        $popularTickets = $this->ticketRepository->getPopularTickets(4);
+        $newTickets = $this->ticketRepository->getAllNewTickets();
+
+        return compact('categories', 'popularTickets', 'newTickets');
+    }
+}
