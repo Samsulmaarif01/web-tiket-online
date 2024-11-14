91e3ab26737eba2842475c81b1d1bf452150d6d9
samsulmaarif1076 <samsulmaarif1076@gmail.com>
2024-11-14 02:26:53 +0000

    feat: make coding seller repository

--- app/Services/FrontService.php
+++ app/Services/FrontService.php
@@ -4,24 +4,28 @@ namespace App\Services;
 
 use App\Repositories\Contracts\TicketRepositoryInterface;
 use App\Repositories\Contracts\CategoryRepositoryInterface;
+use App\Repositories\Contracts\SellerRepositoryInterface;
 
 class FrontService
 {
     protected $categoryRepository;
     protected $ticketRepository;
+    protected $sellerRepository;
 
-    public function __construct(TicketRepositoryInterface $ticketRepository, CategoryRepositoryInterface $categoryRepository)
+    public function __construct(TicketRepositoryInterface $ticketRepository, CategoryRepositoryInterface $categoryRepository, SellerRepositoryInterface $sellerRepository)
     {
         $this->categoryRepository = $categoryRepository;
         $this->ticketRepository = $ticketRepository;
+        $this->sellerRepository = $sellerRepository;
     }
 
     public function getFrontPageData()
     {
         $categories = $this->categoryRepository->getAllCategories();
+        $sellers = $this->sellerRepository->getAllSellers();
         $popularTickets = $this->ticketRepository->getPopularTickets(4);
         $newTickets = $this->ticketRepository->getAllNewTickets();
 
-        return compact('categories', 'popularTickets', 'newTickets');
+        return compact('categories', 'sellers',  'popularTickets', 'newTickets');
     }
 }
