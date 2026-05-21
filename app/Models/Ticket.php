ceebecedaa1120762a8464b0ad675088deb1909e
Samsul Maarif <samsulmaarif1076@gmail.com>
2026-05-21 10:52:31 +0700

    feat: initialize Category, Seller, Ticket, TicketPhoto, and BookingTransaction Eloquent models

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,9 +2,48 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Ticket extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'address',
+        'thumbnail',
+        'path_video',
+        'price',
+        'is_popular',
+        'about',
+        'open_time_at',
+        'closed_time_at',
+        'category_id',
+        'seller_id',
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = ($value);
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function seller()
+    {
+        return $this->belongsTo(Seller::class, 'seller_id');
+    }
+
+    public function photos()
+    {
+        return $this->hasMany(TicketPhoto::class);
+    }
 }
